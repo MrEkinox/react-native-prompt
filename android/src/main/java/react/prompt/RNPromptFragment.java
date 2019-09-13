326c82588115b922f850864814c0fd364db52717
Yannis Caussade <yannis.caussade@gmail.com>
2019-09-13 16:55:53 +0200

    Cursor color

--- android/src/main/java/react/prompt/RNPromptFragment.java
+++ android/src/main/java/react/prompt/RNPromptFragment.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.TextView;
+import 	android.widget.Button;
 
 import android.graphics.Color;
 import android.support.annotation.ColorInt;
@@ -134,12 +135,21 @@ public class RNPromptFragment extends DialogFragment implements DialogInterface.
 
 
             if (arguments.containsKey(ARG_COLOR)) {
-                ColorInt color = Color.parseColor(arguments.getString(ARG_MESSAGE));
-                alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(color);
-                alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(color);
-                alertDialog.getButton(AlertDialog.BUTTON_NEUTRAL).setTextColor(color);
+                int color = Color.parseColor(arguments.getString(ARG_MESSAGE));
+                Button btnNEGATIVE = alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE);
+                Button btnPOSITIVE = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                Button btnNEUTRAL = alertDialog.getButton(AlertDialog.BUTTON_NEUTRAL);
+                if (btnNEGATIVE != null) {
+                    btnNEGATIVE.setTextColor(color);
+                }
+                if (btnPOSITIVE != null) {
+                    btnPOSITIVE.setTextColor(color);
+                }
+                if (btnNEUTRAL != null) {
+                    btnNEUTRAL.setTextColor(color);
+                }
                 Drawable drawable = input.getBackground();
-                drawable.setColorFilter(Color.parseColor(color), PorterDuff.Mode.SRC_ATOP);
+                drawable.setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
                 input.setBackground(drawable);
             }
 
