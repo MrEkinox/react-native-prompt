cb637ce41677eab979c9263127b72170a1de45a9
Yannis Caussade <yannis.caussade@gmail.com>
2019-09-13 16:21:19 +0200

    Cursor color

--- android/src/main/java/react/prompt/RNPromptFragment.java
+++ android/src/main/java/react/prompt/RNPromptFragment.java
@@ -20,6 +20,7 @@ import java.lang.reflect.Field;
 import java.lang.Object;
 import javax.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
+import java.lang.Exception;
 
 public class RNPromptFragment extends DialogFragment implements DialogInterface.OnClickListener {
 
@@ -137,25 +138,30 @@ public class RNPromptFragment extends DialogFragment implements DialogInterface.
     }
 
     public static void setCursorColor(EditText view, @ColorInt int color) {
-        // Get the cursor resource id
-        Field field = TextView.class.getDeclaredField("mCursorDrawableRes");
-        field.setAccessible(true);
-        int drawableResId = field.getInt(view);
-      
-        // Get the editor
-        field = TextView.class.getDeclaredField("mEditor");
-        field.setAccessible(true);
-        Object editor = field.get(view);
-      
-        // Get the drawable and set a color filter
-        Drawable drawable = ContextCompat.getDrawable(view.getContext(), drawableResId);
-        drawable.setColorFilter(color, PorterDuff.Mode.SRC_IN);
-        Drawable[] drawables = {drawable, drawable};
-      
-        // Set the drawables
-        field = editor.getClass().getDeclaredField("mCursorDrawable");
-        field.setAccessible(true);
-        field.set(editor, drawables);
+        try {
+            // Get the cursor resource id
+            Field field = TextView.class.getDeclaredField("mCursorDrawableRes");
+            field.setAccessible(true);
+            int drawableResId = field.getInt(view);
+          
+            // Get the editor
+            field = TextView.class.getDeclaredField("mEditor");
+            field.setAccessible(true);
+            Object editor = field.get(view);
+          
+            // Get the drawable and set a color filter
+            Drawable drawable = ContextCompat.getDrawable(view.getContext(), drawableResId);
+            drawable.setColorFilter(color, PorterDuff.Mode.SRC_IN);
+            Drawable[] drawables = {drawable, drawable};
+          
+            // Set the drawables
+            field = editor.getClass().getDeclaredField("mCursorDrawable");
+            field.setAccessible(true);
+            field.set(editor, drawables);
+
+        } catch (Exception ignored) {
+
+        }
     }
 
     @Override
